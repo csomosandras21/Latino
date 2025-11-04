d1a63ea57e0949a6f96af51fb11d907715e30038
andras <90175859+csomosandras21@users.noreply.github.com>
2025-11-04 12:01:21 +0100

    2025.11.04
    2025.11.04

--- backend/public/js/adatok.js
+++ backend/public/js/adatok.js
@@ -1,4 +1,4 @@
-let markak = ['Armani', 'Boss', 'Burberry', 'Creed', 'Dior', 'Gucci'];
+let markak = ['Armani', 'Boss', 'Burberry', 'Creed', 'Dior', 'Gucci', 'Valentino', 'Jean Paul', 'Versace', 'YSL Saint Laurent', 'Dolce & Gabbana', 'Lattafa'];
 let kedvezmenyek = [0, 10, 20, 30];
 
 module.exports = { markak, kedvezmenyek };
